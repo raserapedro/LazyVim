27f7f9af68cd7a661d7dfd8fbb1b854990e65a18
Pedro Rasera <pedro.rasera@capim.com.br>
2026-03-03 07:40:20 -0300

    high contrast colorscheme options

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,7 +1,16 @@
 return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
-
+  { "ellisonleao/gruvbox.nvim", priority = 1000 },
+  { "scottmckendry/cyberdream.nvim", priority = 1000 },
+  { "miikanissi/modus-themes.nvim", priority = 1000 },
+  {
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = true, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("github-theme").setup({})
+    end,
+  },
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
